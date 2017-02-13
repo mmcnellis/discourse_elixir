2afb50825ac2b04fef48d71c638a34f4b8c2b811
Molloy McNellis <molloy.mcnellis@gmail.com>
2017-02-13 12:30:45 -0600

    Initial commit

---
+++ lib/discourse_elixir.ex
@@ -0,0 +1,18 @@
+defmodule DiscourseElixir do
+  @moduledoc """
+  Documentation for DiscourseElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DiscourseElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
